4f128017efca3d4232e8eeace205e89b2dc36c97
caarloshenrique <carlos.silva@novaandradina.org>
2020-06-06 21:31:37 -0300

    add backend validation

--- ecoleta-api/src/routes.ts
+++ ecoleta-api/src/routes.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { celebrate, Joi } from "celebrate";
+
 import multer from "multer";
 import multerConfig from "./config/multer";
 
@@ -13,7 +15,28 @@ const itemsController = new ItemsController();
 
 routes.get("/items", itemsController.index);
 
-routes.post("/points", upload.single("image"), pointsController.create);
+routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    }
+  ),
+  pointsController.create
+);
 
 routes.get("/points", pointsController.index);
 routes.get("/points/:id", pointsController.show);
